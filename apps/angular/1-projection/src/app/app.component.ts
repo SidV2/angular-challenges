d75d8ffb942350415c0d9658504e9d2013177b5a
Sidhartha.Verma <sidharthv91@gmail.com>
2024-06-05 10:13:40 -0300

    feat(Answer 48): improved angular 18

--- apps/angular/1-projection/src/app/app.component.ts
+++ apps/angular/1-projection/src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { CityCardComponent } from './component/city-card/city-card.component';
 import { StudentCardComponent } from './component/student-card/student-card.component';
 import { TeacherCardComponent } from './component/teacher-card/teacher-card.component';
 
@@ -9,10 +8,9 @@ import { TeacherCardComponent } from './component/teacher-card/teacher-card.comp
     <div class="grid grid-cols-3 gap-3">
       <app-teacher-card></app-teacher-card>
       <app-student-card></app-student-card>
-      <app-city-card></app-city-card>
     </div>
   `,
   standalone: true,
-  imports: [TeacherCardComponent, StudentCardComponent, CityCardComponent],
+  imports: [TeacherCardComponent, StudentCardComponent],
 })
 export class AppComponent {}
